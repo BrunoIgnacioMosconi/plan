9b8eb6fc84722d5f024da5555e6e5574811ce1d8
BrunoIgnacioMosconi <b.i.mosconi@gmail.com>
2025-07-21 21:34:54 -0300

    made it pwa

--- app.js
+++ app.js
@@ -715,3 +715,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
   renderOpcionesForm();
 });
+
+// — Registrar Service Worker —
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(reg => console.log('SW registrado', reg))
+    .catch(err => console.error('SW fallo', err));
+}
